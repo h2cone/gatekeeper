6ab9273c5778e0e20608e65e9193c618d45fa342
h2cone <tw8ape@gmail.com>
2024-09-24 21:10:13 +0800

    Support load balancing

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ A simple HTTP proxy server.
 
 ## Getting Started
 
-To get a local copy up and running, follow these simple steps.
-
 ### Prerequisites
 
 This project requires Rust and Cargo to be installed on your machine. You can download and install them from the [official Rust website](https://www.rust-lang.org/tools/install).
@@ -13,33 +11,33 @@ This project requires Rust and Cargo to be installed on your machine. You can do
 ### Installation
 
 1. Clone the repo
-```bash
-git clone https://github.com/h2cone/gatekeeper
-```
+    ```bash
+    git clone https://github.com/h2cone/gatekeeper
+    ```
 
 2. Navigate to the project directory
-```bash
-cd gatekeeper
-```
+    ```bash
+    cd gatekeeper
+    ```
 
 3. Build the project
-```bash
-cargo build
-```
+    ```bash
+    cargo build -r
+    ```
 
 ## Usage
 
-You can run the server using the `cargo run` command followed by the necessary flags. Here is an example:
+You can run the server via the following command:
 
 ```bash
-cargo run -- -c conf.yaml --ba 0.0.0.0:8008 --pa 127.0.0.1:3000
+RUST_LOG=INFO ./target/release/gatekeeper -c ./conf.yaml --ba 0.0.0.0:8008 --ua 127.0.0.1:3000 127.0.0.1:3001
 ```
 
 ## Command Line Arguments
 
-- `-c <FILE>`: Sets a custom config file.
-- `-d`: Enables daemon mode.
-- `--ba <BIND_ADDRESS>`: Sets the bind address for the server.
-- `--cp <CONTEXT_PATH>`: Sets the context path for the server.
-- `--pa <PEER_ADDRESS>`: Sets the peer address for the server.
-- ...
+- `-c <CONF_FILE>` Sets a custom config file.
+- `--ba <BIND_ADDRESS>` Sets the bind address for the server.
+- `--ua <UPSTREAMS>` Sets the upstreams for the server.
+- `--cp <CONTEXT_PATH>` Sets the context path for the server.
+- `-d` Enables daemon mode.
+- For more information try `--help`.
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
+// @author h2cone
+
 use async_trait::async_trait;
 use pingora::http::RequestHeader;
-use pingora::prelude::{HttpPeer, Opt};
+use pingora::lb::{health_check, LoadBalancer};
+use pingora::prelude::{background_service, HttpPeer, Opt, RoundRobin};
 use pingora::proxy::{http_proxy_service, ProxyHttp, Session};
 use pingora::server::Server;
+use std::sync::Arc;
+use std::time::Duration;
 use structopt::StructOpt;
 
 fn main() {
+    env_logger::init();
+
     let app = App::from_args();
     let mut server = Server::new(Some(app.opt)).unwrap();
     server.bootstrap();
 
-    let mut proxy = http_proxy_service(&server.configuration, app.gateway);
-    proxy.add_tcp(app.bind_addr.as_str());
-    server.add_service(proxy);
+    let mut gateway = app.gateway;
+    let mut lb = LoadBalancer::<RoundRobin>::try_from_iter(&gateway.upstreams).unwrap();
 
+    let hc = health_check::TcpHealthCheck::new();
+    lb.set_health_check(hc);
+    lb.health_check_frequency = Some(Duration::from_secs(1));
+
+    let background = background_service("health-check", lb);
+    let task = background.task();
+    gateway.lb = Some(task);
+
+    let mut proxy = http_proxy_service(&server.configuration, gateway);
+    proxy.add_tcp(app.bind_addr.as_str());
+
+    server.add_service(proxy);
+    server.add_service(background);
     server.run_forever();
 }
 
@@ -32,12 +51,14 @@ pub struct App {
 
 #[derive(StructOpt)]
 pub struct Gateway {
+    #[structopt(skip = None)]
+    lb: Option<Arc<LoadBalancer<RoundRobin>>>,
     /// Context path
     #[structopt(long = "cp", default_value = "/")]
     ctx_path: String,
-    /// Peer address
-    #[structopt(long = "pa")]
-    peer_addr: String,
+    /// Upstream address
+    #[structopt(long = "ua")]
+    upstreams: Vec<String>,
     /// TLS
     #[structopt(long)]
     tls: bool,
@@ -56,13 +77,27 @@ impl ProxyHttp for Gateway {
         Ctx()
     }
 
-    async fn upstream_peer(&self, _session: &mut Session, _ctx: &mut Self::CTX) -> pingora::Result<Box<HttpPeer>> {
-        let peer = HttpPeer::new(self.peer_addr.as_str(), self.tls, self.sni.to_string());
+    async fn upstream_peer(
+        &self,
+        _session: &mut Session,
+        _ctx: &mut Self::CTX,
+    ) -> pingora::Result<Box<HttpPeer>> {
+        let upstream = self.lb.as_ref().unwrap().select(b"", 256).unwrap();
+        let peer = HttpPeer::new(upstream, self.tls, self.sni.to_string());
         return Ok(Box::new(peer));
     }
 
-    async fn request_filter(&self, _session: &mut Session, _ctx: &mut Self::CTX) -> pingora::Result<bool> where Self::CTX: Send + Sync {
-        if self.ctx_path.as_str() == "/" || check_uri(&_session.req_header(), self.ctx_path.as_str()) {
+    async fn request_filter(
+        &self,
+        _session: &mut Session,
+        _ctx: &mut Self::CTX,
+    ) -> pingora::Result<bool>
+    where
+        Self::CTX: Send + Sync,
+    {
+        if self.ctx_path.as_str() == "/"
+            || check_uri(&_session.req_header(), self.ctx_path.as_str())
+        {
             return Ok(false);
         }
         let _ = _session.respond_error(404).await;
